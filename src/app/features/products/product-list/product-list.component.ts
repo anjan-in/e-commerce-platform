4a299a24a80299f3f88886ffb40c8c6547cf16a8
anjan-in <anjansen30@gmail.com>
2025-02-21 23:46:14 +0530

    Added sidebar filters for category and price

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -8,13 +8,31 @@ import { Product, ProductService } from 'src/app/core/services/product.service';
 })
 export class ProductListComponent {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories: string[] = [];
+  selectedCategory: string | null = null;
+  maxPrice: number = 1000;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getProducts().subscribe(data => {
+    this.productService.getProducts().subscribe((data) => {
       this.products = data;
+      this.filteredProducts = data;
+      this.categories = [...new Set(data.map((product) => product.category))]; // Get unique categories
     });
+  }
+
+  filterByCategory(category: string) {
+    this.selectedCategory = category;
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    this.filteredProducts = this.products.filter((product) =>
+      (this.selectedCategory ? product.category === this.selectedCategory : true) &&
+      product.price <= this.maxPrice
+    );
   }
 
 }
--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ProductCardComponent } from './product-card/product-card.component';
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    FormsModule
   ]
 })
 export class ProductsModule { }
